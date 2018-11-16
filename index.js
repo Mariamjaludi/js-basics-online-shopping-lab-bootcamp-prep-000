3588f03574d7e0f9bc7c92312f082336e74953f7
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-16 18:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,37 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
  // write your code here
+ cartObject = {};
+ var price = Math.floor(Math.random() * 100);
+ cartObject.itemName = item;
+ cartObject.itemPrice = price;
+ cart.push(cartObject);
+ return 
 }
 
 function viewCart() {
   // write your code here
+  if (cart.lenth === 0){
+    return "Your shopping cart is empty.";
+  }
+  else{
+    var sentence = "In your cart, you have ";
+    for(let i=0; i< cart.length; i++){
+      if (cart.length === 1){
+      sentence = sentence + `${cart[i].itemName} at $${cart[i].itemPrice}.`;
+      return sentence;
+      }
+      else if (i === cart.length - 1){
+        sentence = sentence + `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+        return sentence;
+      }
+      else{
+          sentence = sentence + `${cart[i].itemName} at $${cart[i].itemPrice},`;
+      }
+  }
 }
 
 function total() {
