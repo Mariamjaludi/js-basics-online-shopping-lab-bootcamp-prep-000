898b070df923817f3d1f4a3fc6ea7ddcbfecb164
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-16 19:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,5 +69,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   var totalCost = total();
-  return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
+  var sentence = `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
+  cart.splice(0);
+  return sentence;
 }
