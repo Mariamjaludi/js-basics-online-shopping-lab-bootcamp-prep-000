9b632bc29fba3f17fb31991a3564594cae33a03e
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-16 18:48:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,13 +47,20 @@ function total() {
   // write your code here
   var total = 0;
   for (let i=0; i<cart.length; i++){
-    total = total + cart[i].itemPrice; 
+    total = total + cart[i].itemPrice;
   }
   return total;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (let i= 0; i< cart.length; i++){
+    if (cart[i].itemName === item){
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
